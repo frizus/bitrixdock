496f2e04e8f6ded50b0c109ea002b166b49096a6
Daniil Tukmakov <irfzus@gmail.com>
2024-12-22 17:36:30 +0500

    сделал директорию битрикс в `www/public` вместо `www`, чтобы в `www` лежал `vendor` и подобное добавил автопроставление в `.env` `SITE_PATH` к абсолютному `www/public`

--- install.sh
+++ install.sh
@@ -1,19 +1,17 @@
 #!/bin/sh
 set -e
 
-echo "Create folder struct"
-mkdir -p /var/www/bitrix && \
-cd /var/www && \
-rm -f /var/www/bitrix/bitrixsetup.php && \
-curl -fsSL https://www.1c-bitrix.ru/download/scripts/bitrixsetup.php -o /var/www/bitrix/bitrixsetup.php && \
-rm -rf /var/www/bitrixdock && \
-git clone --depth=1 https://github.com/bitrixdock/bitrixdock.git && \
-chmod -R 775 /var/www/bitrix && chown -R root:www-data /var/www/bitrix && \
+echo "Скачивание bitrixsetup.php и установка прав"
+mkdir -p ./www/public && \
+rm -f ./www/public/bitrixsetup.php && \
+curl -fsSL https://www.1c-bitrix.ru/download/scripts/bitrixsetup.php -o ./www/public/bitrixsetup.php && \
+sudo chown -R :www-data ./www && chmod -R 2775 ./www
 
-echo "Config"
-cp -f /var/www/bitrixdock/.env_template /var/www/bitrixdock/.env
-sed -i 's/SITE_PATH=.\/www/SITE_PATH=\/var\/www\/bitrix/' /var/www/bitrixdock/.env
+echo "Копирование файла .env"
+cp -n .env.default .env
+sed -i 's,SITE_PATH=.*,SITE_PATH='"$(pwd)"'\/www\/public\/,' ./.env
 
-echo "Run"
-docker compose -p bitrixdock down
-docker compose -f /var/www/bitrixdock/docker-compose.yml -p bitrixdock up -d
+echo "Запуск"
+docker compose down --remove-orphans
+docker compose build
+docker compose up -d
